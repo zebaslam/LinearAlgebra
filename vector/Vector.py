22aaacd54b0afaf1bfbfbd35affef7797647349c
Zeb Aslam <xzeb.aslam@gmail.com>
2017-12-10 17:44:10 -0500

    ability to calculate dot product and angle bwtween two vectors (in rads)

--- vector/Vector.py
+++ vector/Vector.py
@@ -1,4 +1,9 @@
+import math
+
+
 class Vector(object):
+    CANNOT_NORMALIZE_ZERO_VECTOR_NSG = 'Cannot normalize the zero vector'
+
     def __init__(self, coordinates):
         try:
             if not coordinates:
@@ -38,7 +43,7 @@ class Vector(object):
         return Vector(map(lambda x: scale * x, self.coordinates))
 
     def magnitude(self):
-        res = [x**2 for x in self.coordinates]
+        res = [x ** 2 for x in self.coordinates]
         return (sum(res)) ** (1 / 2.0)
 
     def normalize(self):
@@ -46,4 +51,21 @@ class Vector(object):
             scale = 1. / self.magnitude()
             return self.scalar(scale)
         except ZeroDivisionError:
-            raise Exception('Cannot normalize the zero vector')
+            raise Exception(self.CANNOT_NORMALIZE_ZERO_VECTOR_NSG)
+
+    def dotProduct(self, v):
+        res = [x * y for x, y in zip(self.coordinates, v.coordinates)]
+        return sum(res)
+
+    def vector_angle(self, v):
+        try:
+            dp = self.dotProduct(v)
+            smag = self.magnitude()
+            vmag = Vector.magnitude(v)
+            return math.acos(dp / (smag * vmag))
+
+        except Exception as e:
+            if str(e) == self.CANNOT_NORMALIZE_ZERO_VECTOR_NSG:
+                raise Exception('Cannot compute an angle with the zero vector')
+            else:
+                raise e
